ad02011b0fed9bab305149295d3ce8a3a39a3119
alexgtimes <143785637+alexgtimes@users.noreply.github.com>
2024-04-17 12:23:27 +0930

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Emerson Docs',
+  tagline: 'our logo looks like pacman.',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
